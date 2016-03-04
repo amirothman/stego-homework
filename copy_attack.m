1f4abcfb7b2139e74c57cf85fd171f40ec8e6cfb
Amir Othman <th3go2@gmail.com>
2016-03-04 13:13:56 +0100

    multiple cover didnt work

--- copy_attack.m
+++ copy_attack.m
@@ -115,63 +115,68 @@ endfunction
 
 clear;
 
-watermark_string = "dfsgdsf dfskgj dfjsgkdfsj gnjksdfeg";
+watermark_string = "df";
 
 %RGB channel
 pos = 1;
 
-disp('get image');
-% cover_image = ones(1136,640,3);
-cover_image = imread("test02.jpg");
-rect = [1 1 10 10];
-[cover_image, rectangle_cropped]=imcrop(cover_image,rect);
+% disp('get image');
+% % cover_image = ones(1136,640,3);
+% cover_image = imread("test02.jpg");
+% rect = [1 1 10 10];
+% [cover_image, rectangle_cropped]=imcrop(cover_image,rect);
 
+image_n = zeros(100,100,3);
 
 for i=1:1:10
-  % disp(size(cover_image));
-  random_pixel_values = randi(255,size(cover_image));
-  % disp(size(random_pixel_values))
-  % a = mod(cover_image + random_pixel_values,255);
 
+  % disp(size(cover_image));
+  cover_image = randi(255,[100,100,3]);
+  % disp('get bitseq');
+  bitstr = toBits(watermark_string);
+  % disp('imbed');
+  WMWork = embedBits(cover_image , bitstr, pos);
+  %file_name = strcat("test01_with_payload.png");
+  % figure('name', "WMWork");imshow(WMWork);
+  %imwrite(WMWork,file_name);
+  % disp('get image');
+  %imWM = imread(file_name);
+  % figure('name', "Cover work");imshow(cover_image);
+  % figure('name', "imWM");imshow(imWM);
+  %filter with averaging
+  g = @(d) exp(-(d./50).^2);
+  imWM_filtered = imsmooth(WMWork, "p&m", 2, 0.065, g);
+  % figure;
+  % imshow(imWM_filtered);
+  %d = imWM_filtered - imWM;
+  %figure('name', "Average - embeded");imshow(d);
+  %figure('name', "Averaged");imshow(imWM_filtered);
+  d = round(abs(WMWork - imWM_filtered));
+  figure('name',int2str(i));
+  imshow(d);
+  % image_n = image_n+d;
+  %disp(d(:,:,1));
+  
 endfor
 
-
-% disp('get bitseq');
-% bitstr = toBits(watermark_string);
-% disp('imbed');
-% WMWork = embedBits(cover_image , bitstr, pos);
-% file_name = strcat("test01_with_payload.png");
-% figure('name', "WMWork");imshow(WMWork);
-% imwrite(WMWork,file_name);
-% disp('get image');
-% imWM = imread(file_name);
-% figure('name', "Cover work");imshow(cover_image);
-% figure('name', "imWM");imshow(imWM);
-% %filter with averaging
-% g = @(d) exp(-(d./50).^2);
-% imWM_filtered = imsmooth(imWM, "p&m", 2, 0.065, g);
-% figure;
-% imshow(imWM_filtered);
-% %d = imWM_filtered - imWM;
-% %figure('name', "Average - embeded");imshow(d);
-% %figure('name', "Averaged");imshow(imWM_filtered);
-% d = imWM - imWM_filtered;
-
-% %d = abs(imWM_filtered - imWM);
-% figure('name', "Embeded - Cover");
+% disp(image_n);
+%d = abs(imWM_filtered - imWM);
+% figure('name', int2str(i));
 % imshow(d);
+% disp(d(:,:,1));
 % disp('get bits');
-% %cat_image = zeros(1136,640,3);
+%cat_image = zeros(1136,640,3);
 % added_cover_mark = d + cover_image;
 % figure('name', "Approx + Cover");imshow(added_cover_mark);
 % bitseq = getBits(added_cover_mark, pos);
-% %bitseq_2 = reshapeD(d,pos);
+%bitseq_2 = reshapeD(d,pos);
 % disp('get string');
 % str = toString(bitseq);
 % disp(str);
-%disp('get string appr.');
-%str_2 = toString(bitseq_2);
+% disp('get string appr.');
+% str_2 = toString(bitseq_2);
 
-%disp(str_2);
+% disp(str_2);
 
-break;  
+
+break;
